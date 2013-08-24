211a65f70214d273a2fcd52522e1deef5b9a170b
yosuke-furukawa <yosuke.furukawa@gmail.com>
2013-08-25 02:28:12 +0900

    Change curl step

--- site-cookbooks/npm-repo/recipes/default.rb
+++ site-cookbooks/npm-repo/recipes/default.rb
@@ -7,31 +7,71 @@
 # All rights reserved - Do Not Redistribute
 #
 
-directory "/usr/local/npmjs" do
-  mode 00666
-  owner "root"
-  group "root"
-  action :create
-end
-
-git "npmjs" do
+git "#{Chef::Config['file_cache_path']}/npmjs.org" do
   repository "git://github.com/isaacs/npmjs.org.git"
   reference "master"
-  destination "/usr/local/npmjs/npmjs.org"
   action :sync
 end
 
+execute "npm install couchapp -g" do
+  command "npm install couchapp -g"
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  action :run
+end
 
-bash "setup_couchapp" do
-  cwd "/usr/local/npmjs/npmjs.org"
+execute "npm install semver" do
+  command "npm install semver"
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  action :run
+end
+
+execute "npm install couchapp" do
+  command "npm install couchapp"
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  action :run
+end
+
+bash "curl put" do
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
   code <<-EOH
-    npm install couchapp -g
-    npm install
     curl -X PUT http://localhost:5984/registry
-    couchapp push repository/app.js http://localhost:5984/registry
-    couchapp push www/app.js http://localhost:5984/registry
-    curl -X DELETE http://localhost:5984/registry
-    curl -X POST http://127.0.0.1:5984/_replicate  -d '{"source":"http://isaacs.iriscouch.com/registry/", "target":"registry", "continuous":true, "create_target":true}' -H "Content-Type: application/json"
   EOH
 end
 
+execute 'push.sh' do
+  command './push.sh'
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  environment({'npm_package_config_couch' => 'http://localhost:5984/registry'})
+  action :run
+end
+
+execute 'load-views.sh' do
+  command './load-views.sh'
+  cwd "#{Chef::Config[:file_cache_path]}/npmjs.org"
+  environment({'npm_package_config_couch' => 'http://localhost:5984/registry'})
+  action :run
+end
+
+bash 'COPY _design/app' do
+  code <<-EOH
+    curl http://localhost:5984/registry/_design/scratch -X COPY -H destination:'_design/app'
+  EOH
+end
+execute "setup_couchapp push repository" do
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  command "couchapp push registry/app.js http://localhost:5984/registry"
+  action :run
+end
+
+execute "setup_couchapp push app.js" do
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  command "couchapp push www/app.js http://localhost:5984/registry"
+  action :run
+end
+
+bash "curl post" do
+  cwd "#{Chef::Config['file_cache_path']}/npmjs.org"
+  code <<-EOH
+    curl -X POST http://localhost:5984/_replicate -d '{"source":"http://isaacs.iriscouch.com/registry/", "target":"registry" }' -H "Content-Type: application/json"
+  EOH
+end
